e84b39a1c9aeab9d66d0131f5aab987450d6d524
Ellis Cheng <ellischeng123@gmail.com>
2023-01-15 05:49:23 +0800

    MVP version

--- main.py
+++ main.py
@@ -1,43 +1,103 @@
-# This is a sample Python script.
+import os
+import subprocess
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
-import numpy as np
 from decimal import *
 
-SPECTRUM = "C:/Users/EC/Downloads/HighPower/spectrum/2023_01_22.txt"
-LIV = "C:/Users/EC/Downloads/HighPower/LIV/2023_01_06_14_22_23.csv"
+HEADER = 'Lookup Low,Lookup High,WL_P,Low A,High A,Low V,High V,Slope\n'
 
 # 比對兩個資料夾下檔案數量，若不相符則不執行並印出警告
 # 取得檔案清單後先依檔名也就是時間排序，之後再依順序配對
 # 輸出 csv sample:
-# 
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    getcontext().rounding = ROUND_FLOOR
+def process_files(target_file: str, specs: list[str], livs: list[str], l_low: int, l_high: int):
+    target = open(target_file, mode='w')
 
-    lookup_low = int(input('lookup_low: '))
-    lookup_high = int(input('lookup_high: '))
+    try:
+        target.write(HEADER)
+        for idx, spec in enumerate(specs):
+            result = calculate(spec, livs[idx], l_low, l_high)
+            target.write(result)
+    finally:
+        target.write('\n')
+        target.close()
 
-    spec = pd.read_csv(filepath_or_buffer=SPECTRUM, skiprows=0)
-    spec_max = spec.idxmax().values[0]
 
-    print(f"WL_P: {spec_max}")
+def calculate(spec: str, liv: str, l_low: int, l_high: int):
+    spec = pd.read_csv(filepath_or_buffer=spec, skiprows=0)
+    wl_p = spec.idxmax().values[0]
+    # print(f"WL_P: {wl_p}")
 
-    liv = pd.read_csv(filepath_or_buffer=LIV, header=1)
-    low = liv.iloc[(liv['L'] - lookup_low).abs().argsort()[:1]]
-    high = liv.iloc[(liv['L'] - lookup_high).abs().argsort()[:1]]
+    liv = pd.read_csv(filepath_or_buffer=liv, header=1)
+    low = liv.iloc[(liv['L'] - l_low).abs().argsort()[:1]]
+    high = liv.iloc[(liv['L'] - l_high).abs().argsort()[:1]]
 
-    print(f"Low A: {low['A'].values[0]}")
-    print(f"High A: {high['A'].values[0]}")
-    print(f"Low V: {low['V'].values[0]}")
-    print(f"High V: {high['V'].values[0]}")
+    low_a = low['A'].values[0]
+    # print(f"Low A: {low_a}")
+
+    high_a = high['A'].values[0]
+    # print(f"High A: {high_a}")
+
+    low_v = low['V'].values[0]
+    # print(f"Low V: {low_v}")
+
+    high_v = high['V'].values[0]
+    # print(f"High V: {high_v}")
 
     slope = (Decimal(high['L'].values[0]) - Decimal(low['L'].values[0])) / \
             (Decimal(high['A'].values[0]) - Decimal(low['A'].values[0]))
-    print(f'slope: {slope : .3f}')
+    # print(f'slope: {slope : .3f}')
+
+    return f"{l_low},{l_high},{wl_p},{low_a},{high_a},{low_v},{high_v},{slope}\n"
 
 
+def main():
+    getcontext().rounding = ROUND_FLOOR
+
+    spectrum_dir = os.path.abspath(input("Enter SPECTRUM files storage dir: "))
+    # spectrum_dir = os.path.abspath('./samples/spectrum')
+    print(spectrum_dir)
+    print()
+    liv_dir = os.path.abspath(input("Enter LIV files storage dir: "))
+    # liv_dir = os.path.abspath('./samples/LIV')
+    print(liv_dir)
+    print()
+
+    spectrum_files = os.listdir(spectrum_dir)
+    # print(spectrum_files)
+
+    liv_files = os.listdir(liv_dir)
+    # print(liv_files)
+
+    if len(spectrum_files) != len(liv_files):
+        raise RuntimeError('Spectrum files count and LIV files count not match.')
+
+    target_dir = os.path.abspath(input("Enter the file with path you wish to save the result file: "))
+    # target_dir = os.path.abspath('./samples')
+    target_file = os.path.join(target_dir, 'result.csv')
+    print()
+
+    lookup_low = int(input('lookup_low: '))
+    # lookup_low = 6
+    print()
+
+    lookup_high = int(input('lookup_high: '))
+    # lookup_high = 9
+    print()
+
+    spectrum_files.sort()
+    liv_files.sort()
+
+    spectrum_files_abs_path = [os.path.join(spectrum_dir, file) for file in spectrum_files]
+    liv_files_abs_path = [os.path.join(liv_dir, file) for file in liv_files]
+
+    process_files(target_file, spectrum_files_abs_path, liv_files_abs_path, lookup_low, lookup_high)
+
+    input(f"Job done, check result in {target_dir}")
+    input("Result folder will be opened after key input")
+    subprocess.Popen(f'explorer {target_dir}')
+
+
+if __name__ == '__main__':
+    main()
